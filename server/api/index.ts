4012a3b0fa87eff27ac739d09a5bed0ca38219cd
Mostafa Othman <mostafamonier13@gmail.com>
2025-06-09 13:34:14 +0200

    Create index.ts

---
+++ server/api/index.ts
@@ -0,0 +1,9 @@
+// server/index.ts
+
+import { handleRequest } from "./api/agentRunner";
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return handleRequest(request);
+  },
+};
